f8a92b882e92efa5b072ac7e4f116baa8575dced
Ian Iversen <ianiversen1@gmail.com>
2023-01-20 20:55:29 -0500

    done: first program

---
+++ first-program/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  
+  console.log('hello world')
+
+  let age: number = 20;
+
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default App
