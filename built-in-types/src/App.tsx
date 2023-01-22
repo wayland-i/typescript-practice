2a1e7e874a5d262e061e697dfafa198bcf8692c1
Ian Iversen <ianiversen1@gmail.com>
2023-01-22 11:21:55 -0500

    done: built-in-types

---
+++ built-in-types/src/App.tsx
@@ -0,0 +1,32 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+
+  console.log('hello')
+
+  let sales = 123_456_789;
+  let course = 'TypeScript';
+  let is_published = true;
+  let level;
+
+  level = 1;
+  level = 'a';
+
+  function render(document) {
+    console.log(document);
+  }
+
+
+
+
+
+  return (
+    <div className="App">
+      <h1>built-in-types</h1>
+    </div>
+  )
+}
+
+export default App
